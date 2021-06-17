55e11e7cccafa00894ba8628a590b914b8da505f
Victor Pedro <victor.pdsousa1@gmail.com>
2021-06-17 19:12:34 -0300

    create views and routes

--- meusEventos/routes/web.php
+++ meusEventos/routes/web.php
@@ -2,27 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/hello-world', function () {
-    return "Hello World!!";
-});
-
-Route::post('/save', function(){
-    
+    return view('hello-world');
 });
 
 

6d36e65d91034b672c1ba5d8d66be8a3bf3b08b9
Victor Pedro <victor.pdsousa1@gmail.com>
2021-07-16 15:13:26 -0300

    rotas dinâmicas

--- meusEventos/routes/web.php
+++ meusEventos/routes/web.php
@@ -11,6 +11,12 @@ Route::get('/hello-world', function () {
     return view('hello-world');
 });
 
+// ? indica que não é um parâmetro não obrigatório
+// assim a varável dinâmica deve ter um valor padrão (= null ou = "Usuário")
+Route::get('/hello/{name?}', function($name = "Usuário"){
+   return 'Hello, '.$name; 
+});
+
 // Route::get, Route::post, Route::put, Route::patch, Route::delete, Route::options
 // dominio.com.br ou 127.0.0.1:8000 = callback na view de acordo com a /
 // verbos HTTP: GET, POST, PUT, PATCH, DELETE e Options

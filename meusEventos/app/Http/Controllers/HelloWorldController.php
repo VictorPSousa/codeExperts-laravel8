6cbf5e15357c58514f3ac6402865375b52689a92
Victor Pedro <victor.pdsousa1@gmail.com>
2021-08-21 22:34:16 -0300

    utilizando controllers para chamar as views

---
+++ meusEventos/app/Http/Controllers/HelloWorldController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloWorldController extends Controller
+{
+    public function helloWorld(){
+        return view('hello-world');
+    }
+
+    public function hello($name = "Teste"){
+        return 'Hello, '.$name; 
+    }
+}
--- meusEventos/routes/web.php
+++ meusEventos/routes/web.php
@@ -7,15 +7,11 @@ Route::get('/', function () {
 });
 
 // function = callback
-Route::get('/hello-world', function () {
-    return view('hello-world');
-});
+Route::get('/hello-world', [\App\Http\Controllers\HelloWorldController::class, 'helloWorld']);
 
 // ? indica que não é um parâmetro não obrigatório
 // assim a varável dinâmica deve ter um valor padrão (= null ou = "Usuário")
-Route::get('/hello/{name?}', function($name = "Usuário"){
-   return 'Hello, '.$name; 
-});
+Route::get('/hello/{name?}', [\App\Http\Controllers\HelloWorldController::class, 'hello']);
 
 // Route::get, Route::post, Route::put, Route::patch, Route::delete, Route::options
 // dominio.com.br ou 127.0.0.1:8000 = callback na view de acordo com a /
